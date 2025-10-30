222bebeae0f002ed3519013f54c18d0a8bef2944
harshmhase87 <harshmhase87@gmail.com>
2025-10-30 12:50:43 +0530

    second commit

--- app.py
+++ app.py
@@ -35,5 +35,11 @@ def logout():
     flash('Logged out successfully', 'info')
     return redirect(url_for('home'))
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
