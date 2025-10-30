be9b0e3ca8e3c8b69221fa0a3ec687250ff42267
harshmhase87 <harshmhase87@gmail.com>
2025-10-30 12:40:47 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'  # session साठी
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    # 🔥 कोणतंही username/password स्वीकारा
+    if username and password:
+        session['username'] = username
+        flash('Login Successful!', 'success')
+        return redirect(url_for('dashboard'))
+    else:
+        flash('Please enter username and password', 'danger')
+        return redirect(url_for('home'))
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        return render_template('dashboard.html', username=session['username'])
+    else:
+        flash('Please login first', 'warning')
+        return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('Logged out successfully', 'info')
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
